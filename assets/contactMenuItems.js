0799a552e0fe265a3c4d711cc173aa4a0578aa25
Guido Asbun <guido@asbun.io>
2024-02-21 17:14:41 -0800

    add links to contact menu

--- assets/contactMenuItems.js
+++ assets/contactMenuItems.js
@@ -12,37 +12,37 @@ const contactMenuItems = [
     title: "Email",
     description: "Email me",
     icon: FaEnvelope,
-    link: "/",
+    link: "mailto:guido@asbun.io",
   },
   {
     title: "LinkedIn",
     description: "My LinkedIn profile",
     icon: FaLinkedinIn,
-    link: "/",
+    link: "https://www.linkedin.com/in/guidoasbun/",
   },
   {
     title: "GitHub",
     description: "My GitHub profile",
     icon: FaGithubSquare,
-    link: "/",
+    link: "https://github.com/guidoasbun",
   },
   {
     title: "Resume",
     description: "My resume",
     icon: FaFileAlt,
-    link: "/",
+    link: "https://asbun365-my.sharepoint.com/:b:/g/personal/guido_asbun_io/Ea8zP7lNl1BFkmbWTYcfjdcBh6wIy1eCa3bku3XIMZdJNw?e=aM0AP8",
   },
   {
     title: "Message",
     description: "Send me a message",
     icon: FaComment,
-    link: "/",
+    link: "https://forms.office.com/r/KQTKPMi8mi?origin=lprLink",
   },
   {
     title: "Instagram",
     description: "My instagram profile",
     icon: FaInstagram,
-    link: "/",
+    link: "https://www.instagram.com/gdrodrigo8/",
   },
 ];
 
